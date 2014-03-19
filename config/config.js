0b1e81661124328808d831cd750c5e9b9ff662f4
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-03-19 22:45:00 +0800

    Adds Odesk Notifier

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ module.exports = {
     app_secret: 'c27140c2ddae4b10',
     access_token: 'cdebcb4a35b14c2cd29245a0ed67317b',
     access_token_secret: 'eb60d02c85adbbda',
-    query: 'rails'
+    query: 'rails',
+    webhook_url: "http://107.170.50.35:5000/hubot/odesk?room=%23ace.dimasuhid%2F%24vey05.ico%3Ba1680c8b95805cc0"
 }
--- worker.js
+++ worker.js
@@ -1,5 +1,25 @@
 var Odeskify = require('./lib/odeskify'),
-    config = require('./config/config')
+    config = require('./config/config'),
+    request = require('request');
+
+var cronJob = require('cron').CronJob,
+    lastJobID = null;
 
 odeskify = new Odeskify(config);
-odeskify.getData();
+
+new cronJob('* 0,10,20,30,40,50 * * * *', function(){
+    odeskify.getFirst(function(data){
+        if(lastJobID !== data.id) {
+            request({
+                url: config.webhook_url,
+                method: "POST",
+                json: {payload: data}
+            }, function(err, resp, body){
+                if (err) throw err;
+                console.log(body);
+            })
+            lastJobID = data.id;
+        }
+    });
+}, null, true);
+
